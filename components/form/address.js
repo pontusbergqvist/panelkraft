72668d217c1aa96a897d88dc598e9c1b4ffd0b6e
Pontus <pontusbergqvist97@gmail.com>
2022-10-19 16:49:49 +0200

    missed import

--- components/form/address.js
+++ components/form/address.js
@@ -1,4 +1,4 @@
-import { FcOk } from "react-icons/fc";
+import { FcOk, FcCancel } from "react-icons/fc";
 import { useState, useEffect } from "react";
 
 const Address = ({ isOk }) => {
@@ -7,37 +7,35 @@ const Address = ({ isOk }) => {
   const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
-    value.length >= 1
-      ? setIsValid(true)
-      : setIsValid(false);
+    value.length >= 1 ? setIsValid(true) : setIsValid(false);
   }, [value]);
 
   return (
     <div className="block text-left my-5">
-    <label className="text-[16px]" htmlFor="name">
-      Adress:
-    </label>
-    <div className="relative">
-      <input
-        name="address"
-        type="text"
-        placeholder="Adress"
-        className="block w-full placeholder:text-black border p-1 border-[#444] rounded placeholder:opacity-40 relative"
-        onChange={(e) => setValue(e.target.value)}
-        value={value}
-        onFocus={() => setIsActive(true)}
-        onBlur={() => setIsActive(false)}
-      />
-      <i className="absolute top-1/2 -translate-y-1/2 right-2">
-        {isValid && !isActive && <FcOk />}
-        {!isValid && !isActive && value && <FcCancel />}
-      </i>
-    </div>
+      <label className="text-[16px]" htmlFor="name">
+        Adress:
+      </label>
+      <div className="relative">
+        <input
+          name="address"
+          type="text"
+          placeholder="Adress"
+          className="block w-full placeholder:text-black border p-1 border-[#444] rounded placeholder:opacity-40 relative"
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+          onFocus={() => setIsActive(true)}
+          onBlur={() => setIsActive(false)}
+        />
+        <i className="absolute top-1/2 -translate-y-1/2 right-2">
+          {isValid && !isActive && <FcOk />}
+          {!isValid && !isActive && value && <FcCancel />}
+        </i>
+      </div>
       {!isValid && !isActive && (value || !isOk) && (
         <p className="text-red-500 text-[14px]">Fyll i din adress</p>
       )}
     </div>
-  )
-}
+  );
+};
 
 export default Address;
